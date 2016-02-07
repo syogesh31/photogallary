d161f10d0ff069607333c31d25eb3b95066a4c37
Yogesh Shinde <syogesh22@gmail.com>
2016-02-08 01:57:33 +0530

    Intial Commit

---
+++ gallary/src/main/java/com/clicker/GallaryApplication.java
@@ -0,0 +1,12 @@
+package com.clicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallaryApplication.class, args);
+	}
+}
